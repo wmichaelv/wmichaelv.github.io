32f26660849709bc5fffd709134bc2403e93befc
Michael <wmichaelv@hotmail.com>
2014-11-19 11:04:30 -0800

    Create operator.js

---
+++ truthtables/operator.js
@@ -0,0 +1,32 @@
+var Operator = {
+	"D": {
+		eval: function(a, b) {
+			return a || b;
+		},
+		precendence: 3
+	},
+	"C": {
+		eval: function(a, b) {
+			return a && b;
+		},
+		precendence: 2
+	},
+	"N": {
+		eval: function(a) {
+			return !a;
+		},
+		precendence: 1
+	},
+	"K": {
+		eval: function(a, b) {
+			return !a || b;
+		},
+		precendence: 4
+	},
+	"B": {
+		eval: function(a, b) {
+			return a === b;
+		},
+		precendence: 5
+	}
+}
