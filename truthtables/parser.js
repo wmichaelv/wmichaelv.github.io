1f1d9700d53af71e34e238d87d60f8e72096e680
Michael <wmichaelv@hotmail.com>
2014-12-10 15:24:09 -0800

    Update parser.js

--- truthtables/parser.js
+++ truthtables/parser.js
@@ -4,67 +4,50 @@ var gNode = [];    // Track node location
 var vCount = 0;    // Track number of variables
 var pCount = 0;    // Track number of paratheses
 var oFlag = false; // Operator
+var start = 0;     // Start SubstringLocation
+
 function parse (text) {
 	/* For there shall be no error */
-	/* Under Construction
+	/* Under construction
 	for (var i = 0; i < text.length; i++) {
 		if (text[i] in Operator) {
 			addOperator(text[i]);
 		} else if (text[i] == "p") {
-			addVariable(text[i]);
+			start = i;
 			while (!isNaN(text[i++]));
 			i--;
+			addVariable(text.substr(start, i - start);
 		} else if (text[i] == "(") {
+			start = i + 1;
 			pCount++;
-			for (var j = i + 1; j < text.length; j++) {
-				if (text[j] == "(") {
-					pCount++;      // Increment counter
-				} else if (text[j] == ")") {
-					pCount--;      // Decrement counter
-				}
-				if (pCount == 0) { // Found the closing paratheis
-					if (j == text.length - 1) {
-						break;
-					}
-					if (myTree == null) {
-						addOperator(text[j+1]);
-						break;
-					}
-				}
+			while (pCount != 0) {
+				i++;
+				if (text[i] == ")") pCount--;
+				if (text[i] == "(") pCount++;
 			}
-			gNode.push(oNode);
-		} else if (text[i] == ")") {
-			gNode.pop();
+			addGroup(text.substr(start, i - start));
 		}
 	}
 	*/
 }
+
 function addOperator(val) {
-	if (gNode.length > 0) {
-		if (oFlag) {
-			addOperatorByPrecedence(oNode, val);
-		} else {
-			addOperatorByPrecedence(gNode[-1], val);
-		}
+	if (myTree == null) {
+		initTree(val);
 	} else {
-		if (myTree == null) {
-			myTree = new Tree(new TreeNode(val));
-			oNode = myTree.head;
-		} else {
-			if (oNode) {
-				if (oFlag) {
-					addOperatorByPrecedence(oNode, val);	
-				} else if (Operator[val].precedence >= Operator[myTree.head.val].precedence) {
-					myTree.push(new TreeNode(val));
-					oNode = myTree.head;
-				} else {
-					myTree.branch(oNode, new TreeNode(val));
-					oNode = oNode.rightNode;
-				}
-			} else {
+		if (oNode) {
+			if (oFlag) {
+				addOperatorByPrecedence(oNode, val);	
+			} else if (Operator[val].precedence >= Operator[myTree.head.val].precedence) {
 				myTree.push(new TreeNode(val));
 				oNode = myTree.head;
+			} else {
+				myTree.branch(oNode, new TreeNode(val));
+				oNode = oNode.rightNode;
 			}
+		} else {
+			myTree.push(new TreeNode(val));
+			oNode = myTree.head;
 		}
 	}
 	oFlag = true;
@@ -72,8 +55,7 @@ function addOperator(val) {
 function addOperatorByPrecedence(head, val) {
 	if (head == null) {
 		if (myTree == null) {
-			myTree = new Tree(new TreeNode(val));
-			oNode = myTree.head;
+			initTree(val);
 		} else {
 			if (myTree.head.val in Operator) {
 				if (Operator[val].precedence >= Operator[myTree.head.val].precedence) {
@@ -98,13 +80,18 @@ function addOperatorByPrecedence(head, val) {
 		}
 	}
 }
+function addGroup(val) {
+
+}
 function addVariable(val) {
 	if (oFlag) {
-		oNode.rightNode = new TreeNode("p" + (vCount).toString());
+		oNode.rightNode = new TreeNode(val);
 	} else {
-		oNode = new TreeNode("p" + (vCount).toString());
-		myTree = new Tree(oNode);
+		initTree(val);
 	}
-	vCount++;
 	oFlag = false;
+}
+function initTree(val) {
+	myTree = new Tree(new TreeNode(val));
+	oNode = myTree.head;
 }
